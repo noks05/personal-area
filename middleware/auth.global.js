a8282bdb9fe4a95b55cba119234b3e87f1524e81
Maks <mavol_55@mail.ru>
2023-12-31 20:39:06 +0200

    fix JSON.parse errror

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,7 +2,7 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   if (to.path !== '/login') {
     if (process.client) {
-      const resp = JSON.parse(localStorage.getItem("authPersonalAreaTest777"))
+      const resp = JSON.parse(localStorage.getItem("authPersonalAreaTest777") || '{value:false}')
       if (!resp.value) {
         return await navigateTo('/login')
       }
